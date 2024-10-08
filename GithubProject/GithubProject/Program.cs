b71133618664d2aa7bef3ea9ec046da90675d76e
dryanakiev <dyanakiev@codingburgas.bg>
2024-10-08 12:31:26 +0300

    Add example issue

--- GithubProject/GithubProject/Program.cs
+++ GithubProject/GithubProject/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello program!");
+        // TODO: Implement a simple program
+        // labels: development, bot
+        Console.WriteLine("Hello world!");
     }
 }
